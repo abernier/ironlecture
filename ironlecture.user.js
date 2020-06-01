9b6d3b746145b481e4864a8753ab9f1f4b04e0df
Antoine BERNIER <antoine.bernier@gmail.com>
2020-06-01 16:30:17 +0200

    0.1.1

--- ironlecture.user.js
+++ ironlecture.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ironlecture
 // @namespace    name.abernier
-// @version      0.1
+// @version      0.1.1
 // @description  Lecture annotations
 // @author       abernier
 // @include      /^https?:\/\/(preview.)?my.ironhack.com\/.*(WDPT.*201909_PAR|WDPT.*202006_PAR).*
